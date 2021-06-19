a66003f73342fafba82ac65a4a2885ff684f23a9
vadimition <vadym.sirenko.triare@gmail.com>
2021-06-19 14:49:09 +0300

    fix: split menu links

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,8 +4,11 @@ import { NavLink } from "react-router-dom";
 const Menu = () => (
   <div>
     <NavLink to={'/'}>Home</NavLink>
+    {' '}
     <NavLink to={'/users'}>Users</NavLink>
+    {' '}
     <NavLink to={'/projects'}>Projects</NavLink>
+    {' '}
     <NavLink to={'/tracker'}>Tracker</NavLink>
   </div>
 );
